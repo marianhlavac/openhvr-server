c0d6e45e25072ffb7c4ff7a8464f28deaaf0ea41
Marián Hlaváč <hlavac95@gmail.com>
2020-04-26 13:25:20 +0200

    Add CORS support

--- routers/router.go
+++ routers/router.go
@@ -6,10 +6,19 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	"github.com/mmajko/openhvr-server/controllers"
 )
 
 func init() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+	}))
+
 	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/devices",
 			beego.NSInclude(&controllers.DevicesController{}),
